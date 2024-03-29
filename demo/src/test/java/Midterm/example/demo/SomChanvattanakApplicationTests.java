2f20289ca48acdaed9bb4cce4d6251b4073fd03b
Anonymous <126799272+Anonymousdesu@users.noreply.github.com>
2024-03-29 11:17:39 +0700

    Add files via upload

---
+++ demo/src/test/java/Midterm/example/demo/SomChanvattanakApplicationTests.java
@@ -0,0 +1,13 @@
+package Midterm.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomChanvattanakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
